ccfc4959a5537984f5a7fab0c0d69f5d0bab705b
SergeyKalutsky <skalutsky@gmail.com>
2022-02-14 21:27:36 +0300

    added plane

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
+from sklearn.preprocessing import scale
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
-# cam = EditorCamera()
+cam = EditorCamera()
 # cam.gravity = 0
 Sky()
+
+class Plane(Entity):
+    def __init__(self):
+        super().__init__(model='assets/plane', scale=0.001)
+        self.texture = '11803_Airplane_body_diff.jpg'
+        self.texture = '11803_Airplane_wing_big_L_diff.jpg'
+        self.texture = '11803_Airplane_wing_big_R_diff.jpg'
+        self.texture = '11803_Airplane_wing_big_R_diff.jpg'
 
 
 class Bird(Entity):
     def __init__(self):
-        super().__init__(model='assets/HUMBIRD', origin=(0,0,-1), parent=camera)
+        super().__init__(model='assets/HUMBIRD', origin=(0,0.05,-0.5), parent=camera)
 
         self.texture = 'HUMBIRD.JPG'
         self.texture = 'HUMBIRD.TIF'
 
     def update(self):
-        print(self.x, self.y, self.z)
+        self.y -= 0.001
         if held_keys['d']:
-            self.x += time.dt
+            self.x += time.dt/10
             camera.rotation_y += 10 * time.dt
         if held_keys['a']:
-            self.x -= time.dt
+            self.x -= time.dt/10
             camera.rotation_y -= 10 * time.dt
         if held_keys['w']:
             self.z += time.dt
         if held_keys['s']:
             self.z -= time.dt
         if held_keys['space']:
-            self.y += time.dt
+            self.y += time.dt/2
 
 
 sphere = Entity(model="sphere", color=color.yellow, position=(10, 5, 5))
 bird = Bird()
+plane = Plane()
 
 
 app.run()
