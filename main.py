c253ecd69515659ebef133967a8a53caeabc2b4f
SergeyKalutsky <skalutsky@gmail.com>
2022-02-27 10:16:38 +0300

    stuff

--- main.py
+++ main.py
@@ -1,50 +1,124 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
+from random import uniform
 
 app = Ursina()
-cam = EditorCamera()
-# cam.gravity = 0
+# cam = EditorCamera()
 Sky()
 
 class Plane(Entity):
-    def __init__(self):
-        super().__init__(model='assets/plane2')
+    def __init__(self, origin, speed):
+        super().__init__(model='assets/plane2', origin=origin, collider='box')
         self.texture = 'plane/Diffuse'
+        self.rotation_y = 180
+        self.rotation_x += 7
+        self.speed = speed
 
     def update(self):
-        pass
+        self.z -= self.speed
+        if self.z <= -150:
+            score.text = str(int(score.text) + 1)
+            self.z = 100
+            self.x = uniform(-6, 6)
+            self.y = uniform(-20, 20)
+        info = self.intersects()
+        print(info.hit)
 
 
 class Bird(Entity):
     def __init__(self):
-        super().__init__(model='assets/HUMBIRD', origin=(0,0.05,-0.5), parent=camera)
-
+        super().__init__(model='assets/HUMBIRD', origin=(0,0.05,-0.5), parent=camera, scale = 16, collider='box')
         self.texture = 'HUMBIRD.JPG'
         self.texture = 'HUMBIRD.TIF'
 
     def update(self):
-        self.y -= 0.001
         if held_keys['d']:
-            self.x += time.dt/100
-            self.rotation_z += 0.2
+            self.x += time.dt
+            self.rotation_z += 1
             camera.rotation_y += 10 * time.dt
         if held_keys['a']:
-            self.x -= time.dt/100
-            self.rotation_z -= 0.2
+            self.x -= time.dt
+            self.rotation_z -= 1
             camera.rotation_y -= 10 * time.dt
         if held_keys['w']:
             self.z += time.dt
-            camera.z += time.dt 
+            camera.z += time.dt
         if held_keys['s']:
             self.z -= time.dt
-            camera.z -= time.dt 
+            camera.z -= time.dt
         if held_keys['space']:
-            self.y += time.dt/2
+            self.y += 0.1
+            camera.y += 0.1
+        self.y -= 0.02
+        camera.y -= 0.02
 
 
-sphere = Entity(model="sphere", color=color.yellow, position=(10, 5, 5))
+#sphere = Entity(model="sphere", color=color.yellow, position=(10, 5, 5))
 bird = Bird()
-plane = Plane()
+for i in range(5):
+   Plane(origin=(uniform(-6, 6), uniform(-20, 20)), speed=uniform(0.10,2))
 
+score = Text(text='0', y=.43, x=-.75, scale=1.5, origin=(0, 0), background=True)
+
+app.run()
+from ursina import *
+from random import uniform
+
+app = Ursina()
+# cam = EditorCamera()
+Sky()
+
+class Plane(Entity):
+    def __init__(self, origin, speed):
+        super().__init__(model='assets/plane2', origin=origin, collider='box')
+        self.texture = 'plane/Diffuse'
+        self.rotation_y = 180
+        self.rotation_x += 7
+        self.speed = speed
+
+    def update(self):
+        self.z -= self.speed
+        if self.z <= -150:
+            score.text = str(int(score.text) + 1)
+            self.z = 100
+            self.x = uniform(-6, 6)
+            self.y = uniform(-20, 20)
+        info = self.intersects()
+        print(info.hit)
+
+
+class Bird(Entity):
+    def __init__(self):
+        super().__init__(model='assets/HUMBIRD', origin=(0,0.05,-0.5), parent=camera, scale = 16, collider='box')
+        self.texture = 'HUMBIRD.JPG'
+        self.texture = 'HUMBIRD.TIF'
+
+    def update(self):
+        if held_keys['d']:
+            self.x += time.dt
+            self.rotation_z += 1
+            camera.rotation_y += 10 * time.dt
+        if held_keys['a']:
+            self.x -= time.dt
+            self.rotation_z -= 1
+            camera.rotation_y -= 10 * time.dt
+        if held_keys['w']:
+            self.z += time.dt
+            camera.z += time.dt
+        if held_keys['s']:
+            self.z -= time.dt
+            camera.z -= time.dt
+        if held_keys['space']:
+            self.y += 0.1
+            camera.y += 0.1
+        self.y -= 0.02
+        camera.y -= 0.02
+
+
+#sphere = Entity(model="sphere", color=color.yellow, position=(10, 5, 5))
+bird = Bird()
+for i in range(5):
+   Plane(origin=(uniform(-6, 6), uniform(-20, 20)), speed=uniform(0.10,2))
+
+score = Text(text='0', y=.43, x=-.75, scale=1.5, origin=(0, 0), background=True)
 
 app.run()
